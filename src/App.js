bd2abb6fd7829ee8fd328f154693f1e06cfa28d5
Monmoy042 <monmoykms.16@gmail.com>
2022-01-18 18:35:53 +0600

    dynamic data pass from an object to a component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,19 @@ const style = {
   backgroundColor: "#282C34",
   color: "#fff",
 };
+const btn = {
+  marginBottom: "2px",
+  height: "40px",
+  width: "150px",
+  backgroundColor: "blue",
+  color: "#fff",
+  outline: "none",
+  border: "1px solid #fff",
+  borderRadius: "5px",
+  cursor: "pointer",
+};
 
+// App Component
 function App() {
   var name = "Roger Federer";
   var player = {
@@ -24,9 +36,23 @@ function App() {
     name: "Novak Djokovic",
     job: "Tennis Player",
   };
+  var productStyle = {
+    backgroundColor: "cyan",
+    color: "#000",
+    border: "2px solid green",
+    marginBottom: "5px",
+    borderRadius: "5px",
+  };
+
+  // Product Object
+  const products = [
+    { title: "Phone", price: "26000 BDT" },
+    { title: "Laptop", price: "55000 BDT" },
+    { title: "TV", price: "45000 BDT" },
+  ];
 
   // player data
-  const sportsman = ['Rafael Nadal','Tiger Woods'];
+  const sportsman = ["Rafael Nadal", "Tiger Woods"];
   return (
     <div className="App">
       <header className="App-header">
@@ -51,6 +77,11 @@ function App() {
         <Person name="Leo Messi" job="Footballer" />
         <Person name={sportsman[0]} job="Tennis Player" />
       </section>
+      <section style={productStyle}>
+        <Product name={products[0].title} price={products[0].price} />
+        <Product name={products[1].title} price={products[1].price} />
+        <Product name={products[2].title} price={products[2].price} />
+      </section>
     </div>
   );
 }
@@ -63,6 +94,21 @@ function Person(props) {
       <div style={{ border: "2px solid gold", marginBottom: "5px" }}>
         <h1>Name: {props.name}</h1>
         <p>Job: {props.job}</p>
+      </div>
+    </>
+  );
+}
+
+// Product info
+function Product(props) {
+  const { name, price } = props;
+  console.log(props);
+  return (
+    <>
+      <div>
+        <h1>Title: {name}</h1>
+        <h3>Price: {price}</h3>
+        <button style={btn}>Buy Now</button>
       </div>
     </>
   );
