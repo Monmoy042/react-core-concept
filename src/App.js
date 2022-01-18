047ce2c9a384a56d52d8e773603a6030744de2f6
Monmoy042 <monmoykms.16@gmail.com>
2022-01-18 14:26:51 +0600

    Dynamic data and Dynamic Style add in reactJS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  var name = "Roger Federer";
+  var player = {
+    name: "Roger Federer",
+    job: "Tennis Player",
+  };
+  var player2 = {
+    name: "Novak Djokovic",
+    job: "Tennis Player",
+  };
+  const headingStyle = {
+    color: "salmon",
+    textAlign: "center",
+  };
+  const textStyle = {
+    color: "tomato",
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!!!!</p>
+        <h1>Hello react</h1>
       </header>
+      <section>
+        {/* Dynamic Data and Dynamic Style in ReactJS */}
+        <p>Hello World!!!!</p>
+        <p>Hello World Again</p>
+        <p>{2 + -5 + 10 * 2 + 3 / (30 + 60 - (40 + 30))}</p>
+        <p>name: {name}</p>
+        <br />
+        <h1 style={headingStyle}>Player Name: {player.name}</h1>
+        <p style={textStyle}>Player's Job: {player.job}</p>
+        <h1 style={headingStyle}>Player Name: {player2.name}</h1>
+        <p style={textStyle}>Player's Job: {player2.job}</p>
+      </section>
     </div>
   );
 }
